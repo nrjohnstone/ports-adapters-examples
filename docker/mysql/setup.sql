3207ca8ece8c009870806c8660667a289f6c7c5d
nrjohnstone <n.johnstone@coolblue.nl>
2017-09-05 18:11:40 +0200

    add mysql to docker compose and book orders schema

---
+++ docker/mysql/setup.sql
@@ -0,0 +1,12 @@
+create table book_orders (
+    order_id CHAR(36) NOT NULL PRIMARY KEY,
+    supplier VARCHAR(60) NOT NULL,
+    state VARCHAR(60) NOT NULL);  
+
+create table book_order_lines ( 
+    order_line_id CHAR(36) NOT NULL PRIMARY KEY,
+    order_id CHAR(36) NOT NULL,
+    title VARCHAR(60) NOT NULL,
+    price DECIMAL NOT NULL,
+    quantity INT NOT NULL
+);
