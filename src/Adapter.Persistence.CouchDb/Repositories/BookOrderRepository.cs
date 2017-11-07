ac489e6d58ae202432f9e20a443134ba6ae3146c
nrjohnstone <n.johnstone@coolblue.nl>
2017-11-07 12:00:50 +0100

    [implement] add initial adapter and test project for couchdb

---
+++ src/Adapter.Persistence.CouchDb/Repositories/BookOrderRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Entities;
+using Domain.Ports.Persistence;
+
+namespace Adapter.Persistence.CouchDb.Repositories
+{
+    public class BookOrderRepository : IBookOrderRepository
+    {
+        public BookOrder Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Store(BookOrder bookOrder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookOrder> GetBySupplier(string supplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookOrder> GetBySupplier(string supplier, BookOrderState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookOrder> GetByState(BookOrderState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BookOrder> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Adapter.Persistence.CouchDb.Tests.Integration/BookOrderRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adapter.Persistence.CouchDb.Repositories;
+using FluentAssertions;
+using Xunit;
+
+namespace Adapter.Persistence.CouchDb.Tests.Integration
+{
+    public class BookOrderRepositoryTests
+    {
+        private BookOrderRepository CreateSut()
+        {
+            return new BookOrderRepository();
+        }
+
+        [Fact]
+        public void CanCreateInstance()
+        {
+            var sut = CreateSut();
+            sut.Should().NotBeNull();
+        }
+
+    }
+}
