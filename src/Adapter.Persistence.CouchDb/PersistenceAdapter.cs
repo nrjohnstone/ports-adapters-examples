b993fb41af5cf427f14c3cf205d95301241e83f9
nrjohnstone <n.johnstone@coolblue.nl>
2017-12-02 09:52:51 +0100

    [imlpement] create database on startup if it does not exist and create views

--- src/Adapter.Persistence.CouchDb/PersistenceAdapter.cs
+++ src/Adapter.Persistence.CouchDb/PersistenceAdapter.cs
@@ -1,5 +1,9 @@
-﻿using Adapter.Persistence.CouchDb.Repositories;
+﻿using System;
+using System.Net;
+using Adapter.Persistence.CouchDb.Repositories;
+using Adapter.Persistence.CouchDb.Views;
 using Domain.Ports.Persistence;
+using MyCouch;
 using SimpleInjector;
 
 namespace Adapter.Persistence.CouchDb
@@ -7,23 +11,46 @@ namespace Adapter.Persistence.CouchDb
     public class PersistenceAdapter
     {
         private readonly PersistenceAdapterSettings _settings;
+        private readonly CouchDbSettings _couchDbSettings;
 
         public PersistenceAdapter(PersistenceAdapterSettings settings)
         {
             _settings = settings;
+            _couchDbSettings = new CouchDbSettings(
+                _settings.DatabaseUri, _settings.DatabaseName);
         }
 
         public void Initialize()
         {
-            
+            CreateDatabaseIfNotExists();
+            CrewViews();
         }
 
         public void Register(Container container)
         {
-            CouchDbSettings couchDbSettings = new CouchDbSettings(
-                _settings.DatabaseUri, _settings.DatabaseName);
-            container.RegisterSingleton(couchDbSettings);
+           
+            container.RegisterSingleton(_couchDbSettings);
             container.Register<IBookOrderRepository, BookOrderRepository>();
         }
+
+        private void CreateDatabaseIfNotExists()
+        {
+            using (var couchDb = new MyCouchServerClient(_couchDbSettings.DatabaseUri))
+            {
+                if (couchDb.Databases.GetAsync(_couchDbSettings.DatabaseName).Result.StatusCode == HttpStatusCode.OK)
+                    return;
+
+                var response = couchDb.Databases.PutAsync(_couchDbSettings.DatabaseName);
+
+                if (response.Result.StatusCode != HttpStatusCode.Created)
+                    throw new InvalidOperationException("Unable to create database");
+            }
+        }
+
+        private void CrewViews()
+        {
+            new ViewManager(_settings.DatabaseUri, _couchDbSettings.DatabaseName).CreateViews();
+        }
+
     }
 }
