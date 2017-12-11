46b7825a210d3afe5a587776b785a8bcc7da10c3
nrjohnstone <locotewz@gmail.com>
2017-12-11 21:06:38 +0100

    [implement] add REST endpoint for ordering for Client3

--- src/Host.WebService3/BookOrders/BookOrdersController.cs
+++ src/Host.WebService3/BookOrders/BookOrdersController.cs
@@ -2,25 +2,30 @@
 using System.Collections.Generic;
 using System.Web.Http;
 using Domain.UseCases;
+using Domain.ValueObjects;
 
 namespace Host.WebService.Client3.BookOrders
 {
     public class BookOrdersController : ApiController
     {
+        private readonly OrderBookUseCase _orderBookUseCase;
         private readonly ApproveBookOrderUseCase _approveBookOrderUseCase;
         private readonly SendBookOrderUseCase _sendBookOrderUseCase;
         private readonly GetBookOrdersUseCase _getBookOrdersUseCase;
         private readonly DeleteBookOrdersUseCase _deleteBookOrdersUseCase;
 
-        public BookOrdersController(ApproveBookOrderUseCase approveBookOrderUseCase,
+        public BookOrdersController(OrderBookUseCase orderBookUseCase,
+            ApproveBookOrderUseCase approveBookOrderUseCase,
             SendBookOrderUseCase sendBookOrderUseCase,
             GetBookOrdersUseCase getBookOrdersUseCase,
             DeleteBookOrdersUseCase deleteBookOrdersUseCase)
         {
+            if (orderBookUseCase == null) throw new ArgumentNullException(nameof(orderBookUseCase));
             if (approveBookOrderUseCase == null) throw new ArgumentNullException(nameof(approveBookOrderUseCase));
             if (sendBookOrderUseCase == null) throw new ArgumentNullException(nameof(sendBookOrderUseCase));
             if (getBookOrdersUseCase == null) throw new ArgumentNullException(nameof(getBookOrdersUseCase));
             if (deleteBookOrdersUseCase == null) throw new ArgumentNullException(nameof(deleteBookOrdersUseCase));
+            _orderBookUseCase = orderBookUseCase;
             _approveBookOrderUseCase = approveBookOrderUseCase;
             _sendBookOrderUseCase = sendBookOrderUseCase;
             _getBookOrdersUseCase = getBookOrdersUseCase;
@@ -33,7 +38,23 @@ namespace Host.WebService.Client3.BookOrders
         {
             return Ok();
         }
-       
+
+        [HttpPost]
+        [Route("bookRequests")]
+        public IHttpActionResult CreateBookRequest([FromBody] BookTitleOrderRequest bookTitleOrderRequest)
+        {
+            if (bookTitleOrderRequest == null)
+                return BadRequest();
+
+            Guid bookOrderId = _orderBookUseCase.Execute(new BookTitleOrder(
+                bookTitleOrderRequest.Title,
+                bookTitleOrderRequest.Supplier,
+                bookTitleOrderRequest.Price,
+                bookTitleOrderRequest.Quantity));
+
+            return Created<string>($"bookOrders/{bookOrderId}", null);
+        }
+
         [HttpGet]
         [Route("bookOrders")]
         public IHttpActionResult GetBookOrders()
