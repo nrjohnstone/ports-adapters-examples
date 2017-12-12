0e447df2e41220095d8b02883ff40737790dbad3
nrjohnstone <locotewz@gmail.com>
2017-12-12 07:23:42 +0100

    [implement] modify csv polling to load all files matching mask in temporary folder

--- src/Adapter.Trigger.Csv/OrderBookUseCaseTrigger.cs
+++ src/Adapter.Trigger.Csv/OrderBookUseCaseTrigger.cs
@@ -13,45 +13,51 @@ namespace Adapter.Trigger.Csv
         private readonly Thread _threadPoll;
         private readonly ManualResetEvent _shutdownEvent = new ManualResetEvent(false);
         private bool _shutdown;
-        private const string BookOrderFileName = "BookOrders.csv";
-        
+
+        private const string BookOrderFileMask = "BookOrders*.csv";
+        public string BookOrderFileFolder { get; }
+
+
         public OrderBookUseCaseTrigger(OrderBookUseCase orderBookUseCase)
         {
             _orderBookUseCase = orderBookUseCase;
             _threadPoll = new Thread(PollForNewCsv);
-            BookOrderFilePath = Path.Combine(Path.GetTempPath(), BookOrderFileName);
+            BookOrderFileFolder = Path.GetTempPath();
         }
-
-        public string BookOrderFilePath { get; }
 
         private void PollForNewCsv()
         {
             while (!_shutdown)
-            {                
-                if (File.Exists(BookOrderFilePath))
+            {
+                var files = Directory.EnumerateFiles(BookOrderFileFolder, BookOrderFileMask);
+
+                foreach (var file in files)
                 {
-                    var engine = new DelimitedFileEngine<BookTitleOrderModel>();                    
-                    var records = engine.ReadFile(BookOrderFilePath);
-                    
-                    List<BookTitleOrder> bookOrders = new List<BookTitleOrder>();
-
-                    foreach (var record in records)
+                    if (File.Exists(file))
                     {
-                        bookOrders.Add(new BookTitleOrder(
-                            record.Title, record.Supplier, record.Price, record.Quantity));                        
+                        var engine = new DelimitedFileEngine<BookTitleOrderModel>();
+                        var records = engine.ReadFile(file);
+
+                        List<BookTitleOrder> bookOrders = new List<BookTitleOrder>();
+
+                        foreach (var record in records)
+                        {
+                            bookOrders.Add(new BookTitleOrder(
+                                record.Title, record.Supplier, record.Price, record.Quantity));
+                        }
+
+                        // NOTE: Don't do this in production. There is no attempt here do even do this 
+                        // in a transactional manner, and you should mark the file as processed once all 
+                        // orders are placed correctly rather than delete it, and have some kind of support
+                        // for resuming at the last processed line if a fault occurs
+                        File.Delete(file);
+
+                        foreach (var bookOrder in bookOrders)
+                        {
+                            _orderBookUseCase.Execute(bookOrder);
+                        }
                     }
-
-                    // NOTE: Don't do this in production. There is no attempt here do even do this 
-                    // in a transactional manner, and you should mark the file as processed once all 
-                    // orders are placed correctly rather than delete it, and have some kind of support
-                    // for resuming at the last processed line if a fault occurs
-                    File.Delete(BookOrderFilePath);
-
-                    foreach (var bookOrder in bookOrders)
-                    {
-                        _orderBookUseCase.Execute(bookOrder);
-                    }                    
-                }
+                }                
 
                 _shutdownEvent.WaitOne(millisecondsTimeout: 2000);
             }
