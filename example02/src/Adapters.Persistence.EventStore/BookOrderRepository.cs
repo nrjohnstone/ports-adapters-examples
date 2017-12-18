b28e5375e375b9805eb06ac989b813f160166829
nrjohnstone <locotewz@gmail.com>
2017-12-18 07:32:49 +0100

    [implement] add initial implementation of store and get with EventStore

---
+++ example02/src/Adapters.Persistence.EventStore/BookOrderRepository.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Domain.Entities;
+using Domain.Events;
+using Domain.Ports.Persistence;
+using EventStore.ClientAPI;
+using Microsoft.CSharp.RuntimeBinder;
+using Newtonsoft.Json;
+
+namespace Adapters.Persistence.EventStore
+{
+    public class BookOrderRepository : IBookOrderRepository
+    {
+        private const string StreamBaseName = "bookOrders";
+
+        public void Store(BookOrder bookOrder)
+        {
+            using (var connection = EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113)))
+            {
+                connection.ConnectAsync().Wait();
+
+                var events = bookOrder.DequeueAllEvents();
+
+                foreach (var ev in events)
+                {
+                    HandleEvent(ev, connection);
+                }
+            }
+        }
+
+        public BookOrder Get(Guid orderId)
+        {
+            BookOrder bookOrder = null;
+            using (var connection = EventStoreConnection.Create(new IPEndPoint(IPAddress.Loopback, 1113)))
+            {
+                connection.ConnectAsync().Wait();
+
+                StreamEventsSlice currentSlice;
+                long nextSliceStart = StreamPosition.Start;
+                do
+                {                    
+                    currentSlice =
+                        connection.ReadStreamEventsForwardAsync(
+                            $"{StreamBaseName}-{orderId}", nextSliceStart,
+                                200, false).Result;
+
+                    nextSliceStart = currentSlice.NextEventNumber;
+
+                    foreach (var currentSliceEvent in currentSlice.Events)
+                    {
+                        if (currentSliceEvent.Event.EventType.Equals("bookOrderCreated"))
+                        {
+                            var st = Encoding.ASCII.GetString(currentSliceEvent.Event.Data);
+                            BookOrderCreatedEvent ev = JsonConvert.DeserializeObject< BookOrderCreatedEvent>(st);
+                            bookOrder = BookOrder.CreateExisting(ev.Supplier, BookOrderState.New, ev.Id, 
+                                new List<OrderLine>());
+                        }
+                    }
+                    
+                } while (!currentSlice.IsEndOfStream);
+            }
+
+            return bookOrder;
+        }
+
+        private void HandleEvent(IEvent ev, IEventStoreConnection connection)
+        {
+            try
+            {
+                InvokeHandlerUnsafe(ev, connection);
+            }
+            catch (RuntimeBinderException)
+            {
+                throw
+                    new MissingMethodException("No event handler " +
+                                               $"found for event {ev.GetType()}.");
+            }
+        }
+
+        private void InvokeHandlerUnsafe(IEvent ev, IEventStoreConnection connection)
+        {
+            this.Handle((dynamic)ev, connection);
+        }
+
+        private void Handle(BookOrderCreatedEvent ev, IEventStoreConnection connection)
+        {           
+            var myEvent = GetEventDataFor(ev, "bookOrderCreated");
+
+            connection.AppendToStreamAsync($"{StreamBaseName}-{ev.Id}",
+                ExpectedVersion.Any, myEvent).Wait();            
+        }
+
+        private void Handle(BookOrderLineCreatedEvent ev, IEventStoreConnection connection)
+        {
+            var myEvent = GetEventDataFor(ev, "bookOrderLineCreated");
+
+            connection.AppendToStreamAsync($"{StreamBaseName}-{ev.OrderId}",
+                ExpectedVersion.Any, myEvent).Wait();
+        }
+
+        private void Handle(BookOrderLineEditedEvent ev, IEventStoreConnection connection)
+        {
+            var myEvent = GetEventDataFor(ev, "bookOrderLineEdited");
+
+            connection.AppendToStreamAsync($"{StreamBaseName}-{ev.OrderId}",
+                ExpectedVersion.Any, myEvent).Wait();
+        }
+
+        private void Handle(BookOrderLineRemovedEvent ev, IEventStoreConnection connection)
+        {
+            var myEvent = GetEventDataFor(ev, "bookOrderLineRemoved");
+
+            connection.AppendToStreamAsync($"{StreamBaseName}-{ev.OrderId}",
+                ExpectedVersion.Any, myEvent).Wait();
+        }
+
+        public EventData GetEventDataFor<T>(T item, string eventType)
+        {
+            var eventId = Guid.NewGuid();
+            var eventDataFor = new EventData(eventId, eventType, 
+                isJson: true, 
+                data: GetSerializedBytes(item), 
+                metadata: GetSerializedBytes(new { }));
+
+            return eventDataFor;
+        }
+
+        public byte[] GetSerializedBytes<U>(U input)
+        {
+            return Encoding.ASCII.GetBytes(JsonConvert.SerializeObject(input));
+        }
+    }
+}
---
+++ example02/test/Adapters.Persistence.EventStore.Tests/BookOrderRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Domain.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace Adapters.Persistence.EventStore.Tests
+{
+    public class BookOrderRepositoryTests
+    {
+        private BookOrderRepository CreateSut()
+        {
+            return new BookOrderRepository();
+        }
+
+        [Fact]
+        public void Should()
+        {
+            var sut = CreateSut();
+
+            var orderId = Guid.NewGuid();
+            sut.Store(BookOrder.CreateNew("SomeSupplier", orderId));
+
+            var bookOrder = sut.Get(orderId);
+
+            bookOrder.Supplier.Should().Be("SomeSupplier");
+            bookOrder.Id.Should().Be(orderId);
+            bookOrder.State.Should().Be(BookOrderState.New);
+        }
+    }
+}
