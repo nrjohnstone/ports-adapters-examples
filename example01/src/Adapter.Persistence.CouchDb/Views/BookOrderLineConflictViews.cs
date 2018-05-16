79dbfc794996667c1ac313838406335fccfd258e
nrjohnstone <locotewz@gmail.com>
2018-05-16 18:46:44 +0200

    [implement] create new view for all BookOrderLineConflict queries

---
+++ example01/src/Adapter.Persistence.CouchDb/Views/BookOrderLineConflictViews.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace Adapter.Persistence.CouchDb.Views
+{
+    public class BookOrderLineConflictViews : ICouchDbView
+    {
+        public string Id => "_design/" + DesignDocument;
+        public string DesignDocument => "bookorderLineConflicts";
+
+        public string Json()
+        {
+            return JsonConvert.SerializeObject(new
+            {
+                _id = Id,
+                language = "javascript",
+                views = new
+                {
+                    all = new
+                    {
+                        map = "function(doc) { " +
+                              "if(doc.$doctype !== 'bookOrderLineConflictDto' || doc._deleted) return;" +
+                              "emit(doc.id, 0);" +
+                              "}"
+                    }
+                }
+            });
+        }
+    }
+}
