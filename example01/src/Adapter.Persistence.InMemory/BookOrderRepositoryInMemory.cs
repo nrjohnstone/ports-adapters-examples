b0aed274ad8e179e6840fb85c564e5c76182cc0c
Nathan Johnstone <locotewz@gmail.com>
2021-05-21 07:55:33 +0200

    Create in memory persistence adapter to replace use of mocks in unit tests

---
+++ example01/src/Adapter.Persistence.InMemory/BookOrderRepositoryInMemory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Domain.Entities;
+using Domain.Ports.Persistence;
+
+namespace Adapter.Persistence.InMemory
+{
+    public class BookOrderRepositoryInMemory : IBookOrderRepository
+    {
+        private List<BookOrder> _bookOrders = new List<BookOrder>();
+        
+        public BookOrder Get(Guid id)
+        {
+            return _bookOrders.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Store(BookOrder bookOrder)
+        {
+            var existingBookOrder = _bookOrders.FirstOrDefault(x => x.Id == bookOrder.Id);
+
+            if (existingBookOrder != null)
+            {
+                _bookOrders.Remove(existingBookOrder);
+            }
+            
+            _bookOrders.Add(bookOrder);
+        }
+
+        public IEnumerable<BookOrder> GetBySupplier(string supplier)
+        {
+            return _bookOrders.Where(x => x.Supplier == supplier);
+        }
+
+        public IEnumerable<BookOrder> GetBySupplier(string supplier, BookOrderState state)
+        {
+            return _bookOrders.Where(x => x.Supplier == supplier && x.State == state);
+        }
+
+        public IEnumerable<BookOrder> GetByState(BookOrderState state)
+        {
+            return _bookOrders.Where(x => x.State == state);
+        }
+
+        public IEnumerable<BookOrder> Get()
+        {
+            return _bookOrders;
+        }
+
+        public void Delete()
+        {
+            _bookOrders = new List<BookOrder>();
+        }
+    }
+}
